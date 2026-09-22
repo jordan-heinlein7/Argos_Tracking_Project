34b86eff6afedcdc4e93feef65c4f47fb5abae94
jordan-heinlein7 <jjh118@duke.edu>
2026-09-22 18:15:05 -0400

    Add code to define geographic box

--- ArgosSelectionTool.py
+++ ArgosSelectionTool.py
@@ -8,3 +8,12 @@
 # Author: Jordan Heinlein (jjh118@duke.edu)
 # Date:   Fall 2026
 #--------------------------------------------------------------
+
+# Create the geographic selection box for petrel observations as a dictionary
+the_box = {
+    'x_min' : 34.00,
+    'y_min' : -76.00,
+    'x_max' : 34.50,
+    'y_max' : -75.00
+}
+
