ecf7c3ea52b1e209a62339780ced05b181826acd
jordan-heinlein7 <jjh118@duke.edu>
2026-09-23 13:00:09 -0400

    Practice reading in CSV file

---
+++ sidequest1.py
@@ -0,0 +1,12 @@
+#Set a variable to the CSV filename
+the_filename = 'Data/raw/Satellite tracking of black-capped petrels 2019-argos.csv'
+#Create a file object pointing to file name, the mode 'r' stands for read only
+f = open(the_filename,'r')
+
+#Create a list of all the lines in the file via the file object
+line_list = f.readlines()
+#Close the file
+f.close()
+#print the 11th item in the line list
+print(line_list[10])
+
